b03181bea8405868f9b9c47d062098f7d512288f
Melanie Baljko <baljko@yorku.ca>
2025-10-16 13:43:43 -0400

    Incremental changes to quarts config & layouts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Lusitana", // formerly "Schibsted Grotesk"
+        body: "Raleway", // formerly "Source Sans Pro"
+        code: "IBM Plex Mono", // formerly
       },
       colors: {
         lightMode: {
